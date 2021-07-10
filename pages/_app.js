cb91d8bb7b4e340b9c9b2ccdd11db5f7bbe46c64
Sanandh Kumar M <sanandhkumarm@Sanandhs-MacBook-Air.local>
2021-07-11 01:15:44 +0530

    fav

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import Theme from '../styles/theme';
-
+import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/sk.ico" type="image/x-icon" />  
+        <title>Sanandh Kumar M</title>
+      </Head>
       <Theme>
         <Component {...pageProps} />
       </Theme>
